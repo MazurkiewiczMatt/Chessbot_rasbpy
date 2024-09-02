83b8eb89312f4b5d44044a4e3f386ac686ee9341
FutureAncient <lukasz.b.galecki@gmail.com>
2024-09-02 18:21:07 +0200

    Set up main program

--- lattice_sensor.py
+++ lattice_sensor.py
@@ -1,24 +1,35 @@
+"""
+import serial
+import time
+from signal import pause
+from gpiozero import Button, OutputDevice
 
 rows=[2,3,4]
-cols=[22,27,17]
+cols=[22,27,17,]
 rows=[OutputDevice(pin, active_high=True, initial_value=False) for pin in rows]
 columns=[Button(pin, pull_up=True) for pin in cols]
 
 matrix=[[False for _ in range(3)] for _ in range(3)]
+"""
+
 
 def scan_matrix():
-	for i,row in enumerate(rows):
-		row.on()
-		for j,column in enumerate(columns):
-			if column.is_pressed:
-				matrix[i][j]=True
-			else:
-				matrix[i][j]=False
-			print(matrix[i][j])
-		row.off()
-		time.sleep(1)
-	return matrix		
-		
-	scan_matrix()
-	for row in matrix:
-		print(row)
+    """
+    for i,row in enumerate(rows):
+        row.on()
+        for j,column in enumerate(columns):
+            if column.is_pressed:
+                matrix[i][j]=True
+            else:
+                matrix[i][j]=False
+            print(matrix[i][j])
+        row.off()
+        time.sleep(1)
+    return matrix
+
+    scan_matrix()
+    for row in matrix:
+        print(row)
+    """
+    return None
+
--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+from lattice_sensor import scan_matrix
+import time
+
 print("The ChessBot's raspberry Pi software has been launched!")
 
+debug = True
+
+# Variables for debugging
+frame_count = 0
+start_time = time.time()
+
+running = True
+
+while running:
+
+    # Process lattice
+    lattice_reading = scan_matrix()
+    if lattice_reading is not None:
+        print("The state of the board has been updated!")
+        print(lattice_reading)
+
+    # Update timers and do all other stuff
+
+    if debug:
+        # Framerate monitoring
+        frame_count += 1
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= 10.0:
+            average_fps = frame_count / elapsed_time
+            average_frame_time_ms = (elapsed_time / frame_count) * 1000
+            print(f"Average framerate over the last {elapsed_time:.2f} seconds: {average_fps:.2f} FPS")
+            print(f"Average time per frame: {average_frame_time_ms:.2f} ms")
+            frame_count = 0
+            start_time = time.time()
