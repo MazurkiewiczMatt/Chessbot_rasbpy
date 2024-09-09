23f113fd259889b48e206763d1db0d03900eb9ca
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-09-09 23:32:33 +0200

    arduino debugging

--- run.sh
+++ run.sh
@@ -20,27 +20,17 @@ else
     echo "requirements.txt not found. Skipping dependency installation."
 fi
 
-# Check if arduino-cli is installed
-if ! command -v arduino-cli &> /dev/null; then
-    echo "arduino-cli not found. Installing..."
-
-    # Download and install arduino-cli
-    wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linuxarm.tar.gz || { echo "Download failed"; exit 1; }
-    tar -xvf arduino-cli_latest_Linuxarm.tar.gz || { echo "Failed to extract"; exit 1; }
-    sudo mv arduino-cli /usr/local/bin/ || { echo "Failed to move arduino-cli"; exit 1; }
-else
-    echo "arduino-cli is already installed."
-fi
+cd /home/artur/Chessbot_rasbpy/Arduino || { echo "Arduino directory not found"; exit 1; }
 
 # Configure Arduino CLI
-arduino-cli config init || { echo "Config initialization failed"; exit 1; }
-arduino-cli core update-index || { echo "Core index update failed"; exit 1; }
-arduino-cli core install arduino:avr || { echo "Core installation failed"; exit 1; }
+sudo ./arduino-cli config init --overwrite || { echo "Config initialization failed"; exit 1; }
+sudo ./arduino-cli core update-index || { echo "Core index update failed"; exit 1; }
+sudo ./arduino-cli core install arduino:avr || { echo "Core installation failed"; exit 1; }
 
 # Compile and upload the sketch
-cd /home/artur/Chessbot_rasbpy/Arduino || { echo "Sketch directory not found"; exit 1; }
-arduino-cli compile --fqbn arduino:avr:nano . || { echo "Compilation failed"; exit 1; }
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano . || { echo "Upload failed"; exit 1; }
+
+sudo ./arduino-cli compile --fqbn arduino:avr:nano . || { echo "Compilation failed"; exit 1; }
+sudo ./arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:nano . || { echo "Upload failed"; exit 1; }
 
 echo "Arduino setup complete."
 
