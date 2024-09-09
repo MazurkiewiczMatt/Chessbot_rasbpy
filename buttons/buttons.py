9daaee25e12770a5043ff5d2a9f5a0100ecb786a
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-09-09 19:12:40 +0200

    fix

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -2,7 +2,7 @@ class ButtonSensors:
     def __init__(self, dummy=False):
         self.dummy = dummy
         pins = [14, 15, 18, 23, 24, 25, 8, 7]
-	pull_up_pins = [14, 18, 23, 24, 25, 8, 7]  # excluded 15 due to faulty electronics
+        pull_up_pins = [14, 18, 23, 24, 25, 8, 7]  # excluded 15 due to faulty electronics
         if not dummy:
             from gpiozero import Button
             self.buttons = [Button(pin, pull_up=(pin in pull_up_pins)) for pin in pins]
