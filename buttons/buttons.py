f4d235f3c7dbee3c869b9b50ef3f8c5b8029b702
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-09-09 18:52:36 +0200

    remove pull up on faulty pin

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -2,9 +2,10 @@ class ButtonSensors:
     def __init__(self, dummy=False):
         self.dummy = dummy
         pins = [14, 15, 18, 23, 24, 25, 8, 7]
+	pull_up_pins = [14, 18, 23, 24, 25, 8, 7]  # excluded 15 due to faulty electronics
         if not dummy:
             from gpiozero import Button
-            self.buttons = [Button(pin, pull_up=True) for pin in pins]
+            self.buttons = [Button(pin, pull_up=(pin in pull_up_pins)) for pin in pins]
 
     def sense(self):
         if self.dummy:
