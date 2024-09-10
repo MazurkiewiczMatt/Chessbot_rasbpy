6698174d8ac4a588b92595d08be98281a44ac953
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-09-10 19:56:00 +0200

    Correct arduino code

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,10 +1,11 @@
 void setup() {
   Serial.begin(9600);
+  Serial.println("Chessbot software initiated");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String message = Serial.readStringUntil('\\n');
+    String message = Serial.readStringUntil('\n');
     if (message == "PING") {
       Serial.println("PONG");
     }
