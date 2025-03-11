f03e85b5e8efa95214c7807d0c3d493466a96d18
Artur0004 <129087026+Artur0004@users.noreply.github.com>
2025-03-11 18:02:01 +0100

    cd

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/spiesznikrysiek/Desktop/Chessbot/Chessbot_rasbpy
+
 # Pull the latest changes from the git repository
 find .git/objects/ -size 0 -exec rm -f {} \;
 git fetch origin
